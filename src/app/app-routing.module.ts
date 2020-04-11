f2290064ae0f6ac4fd3599feb8676b8b897ba75b
hawrot <mhawro200@caledonian.ac.uk>
2020-04-11 02:35:13 +0100

    Add inbox page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'inbox', pathMatch: 'full' },
+  { path: 'inbox', loadChildren: () => import('./inbox/inbox.module').then( m => m.InboxPageModule)},
+  {
+    path: 'inbox',
+    loadChildren: () => import('./inbox/inbox.module').then( m => m.InboxPageModule)
+  },
 ];
 
 @NgModule({
@@ -13,3 +17,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+/*
+{ path: '', redirectTo: 'home', pathMatch: 'full' },
+{ path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},*/
