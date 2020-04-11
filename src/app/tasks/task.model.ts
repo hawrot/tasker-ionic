45262ad514327f7ef0ee7fddaab30c638dddf968
hawrot <mhawro200@caledonian.ac.uk>
2020-04-12 00:06:52 +0100

    Add task model and task service

---
+++ src/app/tasks/task.model.ts
@@ -0,0 +1,10 @@
+export class Task{
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public createdAt: Date,
+        public dueDate: Date
+    ) {
+    }
+}
