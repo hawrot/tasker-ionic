efd9e34b2bf26c4cd18f65a08f9adc14ce2d6c57
hawrot <mhawro200@caledonian.ac.uk>
2020-04-13 20:39:41 +0100

    Create add-task form

--- src/app/tasks/new-task/new-task.module.ts
+++ src/app/tasks/new-task/new-task.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,13 @@ import { NewTaskPageRoutingModule } from './new-task-routing.module';
 import { NewTaskPage } from './new-task.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    NewTaskPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        NewTaskPageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [NewTaskPage]
 })
 export class NewTaskPageModule {}
--- src/app/tasks/new-task/new-task.page.ts
+++ src/app/tasks/new-task/new-task.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-new-task',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTaskPage implements OnInit {
 
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl({updateOn: 'blur', validators: [Validators.required]}),
+      description: new FormControl({updateOn: 'blur', validators: [Validators.required]}),
+      setDueDate: new FormControl({updateOn: 'blur', validators: [Validators.required]}),
+      setDueTime: new FormControl({updateOn: 'blur', validators: [Validators.required]})
+    })
+  }
+
+  onCreateOffer(){
+    if(!this.form.valid){
+      return;
+    }
+    console.log(this.form);
   }
 
 }
