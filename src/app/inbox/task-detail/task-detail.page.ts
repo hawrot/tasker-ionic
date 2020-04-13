35d66a512c77fcf14b6f1e333fb23b1df18489d2
hawrot <mhawro200@caledonian.ac.uk>
2020-04-13 22:22:06 +0100

    Add subscription to the task details

--- src/app/inbox/task-detail/task-detail.page.ts
+++ src/app/inbox/task-detail/task-detail.page.ts
@@ -1,27 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Task} from "../../tasks/task.model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {TasksService} from "../../tasks/tasks.service";
+import {Subscription} from "rxjs";
+import {NavController} from "@ionic/angular";
 
 @Component({
   selector: 'app-task-detail',
   templateUrl: './task-detail.page.html',
   styleUrls: ['./task-detail.page.scss'],
 })
-export class TaskDetailPage implements OnInit {
+export class TaskDetailPage implements OnInit, OnDestroy {
 
   task: Task;
+  private taskSub: Subscription;
 
-  constructor(private router: Router, private route:ActivatedRoute, private tasksService: TasksService) { }
+  constructor(private router: Router,
+              private route:ActivatedRoute,
+              private tasksService: TasksService,
+              private navController: NavController
+
+              ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap =>{
       if(!paramMap.has('taskId')){
-        console.log('Error');
+       this.navController.navigateBack('/');
       }
-      this.task = this.tasksService.getTasks(paramMap.get('taskId'));
+      this.taskSub = this.tasksService.getTask(paramMap.get('taskId')).subscribe(task =>{
+        this.task = task;
+      })
 
     })
+  }
+
+  ngOnDestroy()  {
+    if(this.taskSub){
+      this.taskSub.unsubscribe();
+    }
   }
 
 }
