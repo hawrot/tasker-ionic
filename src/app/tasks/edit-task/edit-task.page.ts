579df04e8ad8015dabcde9cbaf7235d372b08e50
hawrot <mhawro200@caledonian.ac.uk>
2020-04-15 21:12:55 +0100

    Add logic to display edit menu

--- src/app/tasks/edit-task/edit-task.page.ts
+++ src/app/tasks/edit-task/edit-task.page.ts
@@ -2,6 +2,9 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, ActivationEnd, Router} from "@angular/router";
 import {TasksService} from "../tasks.service";
 import {AlertController, LoadingController, NavController} from "@ionic/angular";
+import {Task} from "../task.model";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-edit-task',
@@ -9,6 +12,12 @@ import {AlertController, LoadingController, NavController} from "@ionic/angular"
   styleUrls: ['./edit-task.page.scss'],
 })
 export class EditTaskPage implements OnInit, OnDestroy {
+
+  task: Task;
+  taskId: string;
+  form: FormGroup;
+  isLoading = false;
+  private taskSub: Subscription;
 
   constructor(
       private route: ActivatedRoute,
@@ -20,7 +29,53 @@ export class EditTaskPage implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-
+    this.route.paramMap.subscribe(paramMap =>{
+      if(!paramMap.has('taskId')){
+        this.navCtrl.navigateBack('/');
+        return;
+      }
+      this.taskId = paramMap.get('taskId');
+      this.isLoading = true;
+      this.taskSub = this.tasksService
+          .getTask(paramMap.get('taskId'))
+          .subscribe(
+              task =>{
+                this.task = task;
+                this.form = new FormGroup({
+                  title: new FormControl(this.task.title,{
+                    updateOn: 'blur',
+                    validators: [Validators.required]
+                  }),
+                  description: new FormControl(this.task.description, {
+                    updateOn: 'blur',
+                    validators: [Validators.required, Validators.maxLength(180)]
+                  }),
+                  completed: new FormControl(this.task.completed, {
+                    updateOn: 'blur',
+                    validators: [Validators.required]
+                  })
+                });
+                this.isLoading = false;
+              },
+              error => {
+                this.altertCtrl.create({
+                  header: 'An error occured',
+                  message: 'Task not found',
+                  buttons: [
+                    {
+                      text: 'Okay',
+                      handler: () => {
+                        this.router.navigate(['/']);
+                      }
+                    }
+                  ]
+                })
+                    .then(alertEl =>{
+                      alertEl.present();
+                    })
+              }
+          )
+    })
   }
 
   ngOnDestroy(): void {
