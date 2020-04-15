447fa68f2b0016bd96b4bd1e7d5cf4da3e397fe8
hawrot <mhawro200@caledonian.ac.uk>
2020-04-15 21:16:10 +0100

    Create edit form

--- src/app/tasks/edit-task/edit-task.module.ts
+++ src/app/tasks/edit-task/edit-task.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,13 @@ import { EditTaskPageRoutingModule } from './edit-task-routing.module';
 import { EditTaskPage } from './edit-task.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    EditTaskPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        EditTaskPageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [EditTaskPage]
 })
 export class EditTaskPageModule {}
