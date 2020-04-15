142b7be1fb35ad52f4a2c862bf4c3e33eb5fbc1f
hawrot <mhawro200@caledonian.ac.uk>
2020-04-15 19:45:02 +0100

    Configure constructor for task-edit

--- src/app/tasks/edit-task/edit-task.page.ts
+++ src/app/tasks/edit-task/edit-task.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, ActivationEnd, Router} from "@angular/router";
+import {TasksService} from "../tasks.service";
+import {AlertController, LoadingController, NavController} from "@ionic/angular";
 
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.page.html',
   styleUrls: ['./edit-task.page.scss'],
 })
-export class EditTaskPage implements OnInit {
+export class EditTaskPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+      private route: ActivatedRoute,
+      private tasksService: TasksService,
+      private navCtrl: NavController,
+      private router: Router,
+      private loadingController: LoadingController,
+      private altertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
+
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
