4c471f15eedb0fbb2b5b85c42daa58a2cc061447
hawrot <mhawro200@caledonian.ac.uk>
2020-04-15 02:55:15 +0100

    Filter due tasks

--- src/app/due/due.page.ts
+++ src/app/due/due.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {TasksService} from "../tasks/tasks.service";
+import {MenuController} from "@ionic/angular";
+import {Task} from "../tasks/task.model";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-due',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DuePage implements OnInit {
 
-  constructor() { }
+  loadedTasks: Task[];
+  private tasksSub: Subscription;
+  isLoading = false;
+  filteredTasks: Task[];
+
+  constructor(private tasksService: TasksService, private menu: MenuController) { }
 
   ngOnInit() {
+    this.tasksSub = this.tasksService.tasks.subscribe(tasks =>{
+      this.loadedTasks = tasks;
+      const today = new Date().setHours(0,0,0,0);
+      this.filteredTasks =  tasks.filter(obj => new Date(obj['dueDate']).setHours(0,0,0,0) <= today)
+    })
   }
+  ionViewWillEnter(){
+    this.isLoading = true;
+    this.tasksService.fetchPlaces().subscribe(()=>{
+      this.isLoading = false;
+
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.tasksSub){
+      this.tasksSub.unsubscribe();
+    }
+  }
+
+  getDate(){
+   return new Date(Date.now());
+  }
+
+
 
 }
